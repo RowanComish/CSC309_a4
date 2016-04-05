c7d10c15be0e3158ef07e811a939bc689f9bfcfd
Abbass-Ali Siwjee <abu.siwjee@utoronto.ca>
2016-04-05 10:28:30 -0400

    Changed how dates are presented

--- app/models/recipes.js
+++ app/models/recipes.js
@@ -3,7 +3,6 @@ var bcrypt   = require('bcrypt-nodejs');
 
 // define the schema for our recipe model
 var recipeSchema = mongoose.Schema({
-
         _id: Number,
         name : String,
         author_id : {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
@@ -18,7 +17,6 @@ var recipeSchema = mongoose.Schema({
         description: String,
         review_avg : Number,
         date: { type: Date, default: Date.now }
-
 });
 
 // create the model for recipes and expose it to our app
