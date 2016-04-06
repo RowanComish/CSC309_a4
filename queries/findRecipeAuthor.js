e43a7e5190cf4a2c5a209545117c308b23fafb86
RowanComish <comish_me@hotmail.com>
2016-04-06 19:23:46 -0400

    Added some comments, changed about

--- queries/findRecipeAuthor.js
+++ queries/findRecipeAuthor.js
@@ -2,7 +2,7 @@
 var Recipe = require('../app/models/recipes');
 
 module.exports =function(recipe_id){
-
+	//get info about the author of a recipe
 	var temp = Recipe.findOne({'_id' : recipe_id}).populate('author_id');
 	return temp;
 
